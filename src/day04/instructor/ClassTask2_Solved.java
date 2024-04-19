3ac4b7c3d8f58d7a1789a9a40884045a6848fc1f
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-04-18 21:34:37 -0400

    task2

--- src/day04/instructor/ClassTask2_Solved.java
+++ src/day04/instructor/ClassTask2_Solved.java
@@ -1,6 +1,13 @@
 package day04.instructor;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class ClassTask2_Solved {
     public static void main(String[] args) {
         /**
@@ -9,6 +16,21 @@ public class ClassTask2_Solved {
          * 2. Print out "Saucedemo" link's text only from the list
          */
 
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/TLA/Selenium/B-8/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/#/home");
+
+        List<WebElement> list = driver.findElements(By.className("link"));
+
+        //1
+        for (WebElement eachItem: list){
+            System.out.println(eachItem.getText());
+        }
+
+        //2
+        System.out.println(list.get(5).getText());
+
+        driver.close();
 
     }
 }
