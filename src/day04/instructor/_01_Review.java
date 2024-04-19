a6970c91f39e5c3d920da3fedf9e1a7431ecfbc3
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-04-18 20:17:24 -0400

    01 and 02 done

--- src/day04/instructor/_01_Review.java
+++ src/day04/instructor/_01_Review.java
@@ -1,5 +1,6 @@
 package day04.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,8 +15,24 @@ public class _01_Review {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/TLA/Selenium/B-8/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.udemy.com/");
 
+        //2
+        System.out.println(driver.findElement(By.linkText("Log in")).isEnabled());
 
+        //3
+        if (driver.getTitle().equals("Online Courses - Learn Anything, On Your Schedule | Udemy")){
+            System.out.println("Title test PASS");
+        }else {
+            System.out.println("Tile test FAIL");
+        }
+
+        //4
+        if (driver.getCurrentUrl().startsWith("https")){
+            System.out.println("URL test PASS");
+        }else {
+            System.out.println("URL test FAIL");
+        }
 
         driver.close();
 
--- src/day04/instructor/_02_Review.java
+++ src/day04/instructor/_02_Review.java
@@ -1,7 +1,11 @@
 package day04.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class _02_Review {
     /**
@@ -16,7 +20,7 @@ public class _02_Review {
         WebDriver driver = new ChromeDriver();
 
         // 1. Open https://www.coolthings.com/
-
+        driver.get("https://www.coolthings.com/");
 
         // 2. Verify title of the page is "CoolThings.com | Cool Stuff, Cool Gadgets, Cool Gifts & Things".
 
@@ -24,18 +28,32 @@ public class _02_Review {
         // 3. Verify each page has expected headers:
         // page: Gadgets -> header: Gadgets, Gifts -> Cool Gifts, Gear -> Gear, Toys -> Toys
         //Option 1: Individual
-
+        driver.findElement(By.linkText("GADGETS")).click();
+        if (driver.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Gadgets")){
+            System.out.println("Header test PASS");
+        }else {
+            System.out.println("Header test FAIL");
+        }
 
         //Option 2: Grouped
+        Map<String, String > data = new HashMap<>();
+        data.put("GADGETS", "Gadgets");
+        data.put("GIFTS", "Gifts");
+        data.put("GEAR", "Gear");
+        data.put("TOYS", "Cool Toys");
+
+        for (String linkText: data.keySet()){
+            driver.findElement(By.linkText(linkText)).click();
+            if (driver.findElement(By.tagName("h1")).getText().equalsIgnoreCase(data.get(linkText))){
+                System.out.println("Header test PASS");
+            }else {
+                System.out.println(data.get(linkText) + " - Header test FAIL");
+                System.out.println("Expected is " + data.get(linkText) + " | Actual was: " + driver.findElement(By.tagName("h1")).getText());
+            }
+        }
 
 
         driver.close();
-
-
-
-
-
-
 
         //NOTE: ClassTask 1
 
