1c0d9e0749c3db5b25759912cf622e42fb88c9cd
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-04-20 11:24:16 -0400

    demo3

--- src/day05/instructor/_04_CssLocatorByAttribute.java
+++ src/day05/instructor/_04_CssLocatorByAttribute.java
@@ -1,5 +1,6 @@
 package day05.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,13 +8,13 @@ public class _04_CssLocatorByAttribute {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/TLA/Selenium/B-8/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://automation.techleadacademy.io/#/");
+        driver.get("https://selenium-practice-app.herokuapp.com/#/home");
 
         // tag + [attribute=value] => print PHP Travels text of the link
-
+        System.out.println(driver.findElement(By.cssSelector("a[href='https://phptravels.com/demo/']")).getText());
 
         //print header
-
+        System.out.println(driver.findElement(By.cssSelector("h1[name=h1-text]")).getText());
 
         driver.close();
 
