4e9c76eb4994c2533703e23df399d991f7fa4099
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-04-21 09:56:13 -0400

    day06 demo

--- src/day06/instructor/_01_Review.java
+++ src/day06/instructor/_01_Review.java
@@ -1,5 +1,10 @@
 package day06.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class _01_Review {
     /**
      * basic selectors
@@ -12,7 +17,24 @@ public class _01_Review {
      */
 
     public static void main(String[] args) {
+        Driver.getDriver().get("https://selenium-practice-app.herokuapp.com/#/home");
 
+        //id
+        Driver.getDriver().findElement(By.cssSelector("#titleValue"));
 
+        //class
+        Driver.getDriver().findElement(By.cssSelector(".classValue"));
+
+        //tag + id
+        Driver.getDriver().findElement(By.cssSelector("h1[id=value]"));
+
+        //tag + class
+        Driver.getDriver().findElement(By.cssSelector("a[class='value1 value2']"));
+
+        //tag + attribute
+        Driver.getDriver().findElement(By.cssSelector("div[data-test=value]"));
+
+        //when we don't want to specify tagName
+        Driver.getDriver().findElement(By.cssSelector("*[name=paragraph]"));
     }
 }
--- src/day06/instructor/_02_WarmUpDemo.java
+++ src/day06/instructor/_02_WarmUpDemo.java
@@ -1,14 +1,31 @@
 package day06.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class _02_WarmUpDemo {
     //0. Inputs page on practice website
     //1. Create an array of String with test data
     //2. Capture all input elements using common locator of the form
     //3. Input test data above using loop
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Driver.getDriver().get("https://selenium-practice-app.herokuapp.com/#/inputs");
 
+        String[] dataArr = {"John", "Smith", "john@test.com", "444-444-4444", "Address", "New York",
+        "New York", "12345"};
 
+        //will locate 3 extra elements from the above of the page
+        List<WebElement> list = Driver.getDriver().findElements(By.cssSelector(".form-control"));
 
+        for(int i = 3; i < list.size(); i++){
+            Thread.sleep(1000);
+            list.get(i).sendKeys(dataArr[i-3]);
+        }
+
+        Driver.getDriver().close();
     }
 
     //NOTE: ClassTask 1
