54f8d93a8d606cdb212b475d18ce8ff27db50d0f
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-04-25 19:13:31 -0400

    day08

--- src/day08/instructor/_01_ReviewCssLocators.java
+++ src/day08/instructor/_01_ReviewCssLocators.java
@@ -1,7 +1,11 @@
 package day08.instructor;
 
 import _utils.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 
 //HOW TO LOCATE AN ELEMENT IN DIFFERENT WAYS?
@@ -11,18 +15,34 @@ public class _01_ReviewCssLocators {
         driver.get("https://selenium-practice-app.herokuapp.com/#/selectors");
 
         //print out Red, Green, Blue and Yellow colors under Car color section
-
+//        System.out.println(driver.findElement(By.cssSelector("#red-color")).getText());
+//        System.out.println(driver.findElement(By.cssSelector("li[name=green-color]")).getText());
+//        System.out.println(driver.findElement(By.cssSelector(".yellow-color")).getText());
 
         //1st option to print "Blue"
-
+        List<WebElement> list1 = driver.findElements(By.cssSelector("li"));
+        System.out.println(list1.size());
+        System.out.println(list1.get(8).getText());
+        System.out.println("-------");
 
         //2nd option
-
+        List<WebElement> list2 = driver.findElements(By.cssSelector(".selectors-flexBox > li"));
+        System.out.println(list2.size());
+        System.out.println(list2.get(3).getText());
+        System.out.println("-------");
 
         //3rd option
-
+        //using NOT keyword to not include specific attribute or tag, or combination of those 2
+        //syntax: locator:not(locator not to include)
+        //tag[attr=value]:not(tag[attr=value]), attr=value:not(attr=value)
+        //Examples: a[class=test]:not(a[class=test2]), #idValue:not(.classValue)
+        List<WebElement> list3 = driver.findElements(By.cssSelector(".selectors-flexBox:not(#ball) > li"));
+        System.out.println(list3.size());
+        System.out.println(list3.get(3).getText());
+        System.out.println("--------");
 
         //4th option
+        System.out.println(driver.findElement(By.cssSelector(".selectors-flexBox:not(#ball) > li:nth-child(4)")).getText());
 
 
         driver.close();
