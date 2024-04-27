17bee31fd562879e073cfeb0b4121afb6124a10b
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-04-27 11:00:28 -0400

    day09

--- src/day09/student/_02_ClassTask.java
+++ src/day09/student/_02_ClassTask.java
@@ -1,12 +1,19 @@
 package day09.student;
 
+import _utils.Driver;
+import org.openqa.selenium.WebDriver;
+
 public class _02_ClassTask {
     public static void main(String[] args) {
         /**
          * Task: print out href value of saucedemo link on homepage using text() option of xpath
          * result: href="http://www.saucedemo.com/"
          */
+        WebDriver driver = Driver.getDriver();
+        driver.manage().window().maximize();
+        driver.get("https://selenium-practice-app.herokuapp.com/#/home");
 
+        //your code goes here:
 
     }
 }
--- src/day09/student/_03_ClassTask.java
+++ src/day09/student/_03_ClassTask.java
@@ -1,11 +1,18 @@
 package day09.student;
 
+import _utils.Driver;
+import org.openqa.selenium.WebDriver;
+
 public class _03_ClassTask {
     public static void main(String[] args) {
         /**
          * Task: print out text of all links that has href attribute value starting with "https"
          */
+        WebDriver driver = Driver.getDriver();
+        driver.manage().window().maximize();
+        driver.get("https://selenium-practice-app.herokuapp.com/#/home");
 
+        //your code goes here:
 
     }
 }
