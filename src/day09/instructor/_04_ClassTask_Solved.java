09ec3739384d72b8232c57267d4a6ad11d6be5ce
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-04-28 11:36:37 -0400

    day09

--- src/day09/instructor/_04_ClassTask_Solved.java
+++ src/day09/instructor/_04_ClassTask_Solved.java
@@ -15,7 +15,7 @@ public class _04_ClassTask_Solved {
          * 2. Click on Pricing nav button
          * 3. Print out all questions under "Frequently asked questions" section
          *
-         * 4. Click on Sign in button
+         * 4. Click on Sign in button from Pricing page
          * 5. Input "test@test.com" in email address field
          * 6. Input "test" in password field
          * 7. Click on "Sign in" button
@@ -34,6 +34,22 @@ public class _04_ClassTask_Solved {
         List<WebElement> list = driver.findElements(By.xpath("//div[contains(@class, ' faq-mktg-item')]"));
         list.forEach(each -> System.out.println(each.getText()));
 
+        //click on Sign in button from the main page
+        driver.findElement(By.xpath("//a[contains(text(),'Sign in')][starts-with(@class, 'HeaderMenu')]")).click();
+
+        //input credentials
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='login_field']")).sendKeys("test@test.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("test");
+
+        driver.findElement(By.xpath("//input[@name='commit']")).click();
+
+        //verify
+        if (driver.findElement(By.xpath("//*[contains(text(), 'Incorrect username or password.')]")).isDisplayed()){
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
 
         driver.close();
