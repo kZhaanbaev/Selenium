9d03445e8be60f0399415c8d2fd38be708b75af5
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-02 18:58:58 -0400

    day12

--- src/day12/instructor/_01_SelectClass.java
+++ src/day12/instructor/_01_SelectClass.java
@@ -1,30 +1,47 @@
 package day12.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class _01_SelectClass {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "path");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = Driver.getDriver();
         driver.manage().window().maximize();
-        driver.get("http://automation.techleadacademy.io/#/selectclass");
+        driver.get("https://selenium-practice-app.herokuapp.com/#/selectclass");
 
         //when to use Select class? (Example: Select number)
+        //in order to use Select class, locator has to have a <select> tag
+        Select select = new Select(driver.findElement(By.name("select1")));
 
         //select by text
+        Thread.sleep(2000);
+        select.selectByVisibleText("Three");
 
         //select by index
+        Thread.sleep(2000);
+        select.selectByIndex(1);
 
         //select by value
+        Thread.sleep(2000);
+        select.selectByValue("Eight");
 
         //how to get all selectable options?
+        List<WebElement> options = select.getOptions();
+        options.forEach(each -> System.out.println(each.getText()));
 
+        //how to get selected option
+        System.out.println("---------");
+        System.out.println(select.getFirstSelectedOption().getText());
 
         //NOTE: ClassTask 1 - 10min
 
 
-        Thread.sleep(3000);
+        Thread.sleep(4000);
         driver.close();
 
     }
