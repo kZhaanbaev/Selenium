b0e703c14b296efee27ad7e19b69130400aec7fc
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-07 20:09:07 -0400

    day15

--- src/day15/instructor/_01_iFrames.java
+++ src/day15/instructor/_01_iFrames.java
@@ -3,7 +3,10 @@ package day15.instructor;
 import _utils.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class _01_iFrames {
     public static void main(String[] args) {
@@ -12,21 +15,30 @@ public class _01_iFrames {
         driver.get("https://selenium-practice-app.herokuapp.com/#/iframes");
 
         //1. access TLA website and print main text "Hello! I am the parent iFrame without switching
-        driver.switchTo().frame("parent-iframe");
-        System.out.println(driver.findElement(By.id("iframe-text")).getText());
-
 
         //2. NOTE: README.md file
 
         //3. access parent iFrame and print text again with switching
+//        driver.switchTo().frame("parent-iframe");
+//        System.out.println(driver.findElement(By.id("iframe-text")).getText());
 
         //4. access child iFrame - budget calc
         //input Transaction name and amount
+        driver.switchTo().frame("parent-iframe");
+        driver.switchTo().frame("budget-tracker");
+        driver.findElement(By.id("t-name")).sendKeys("MacBook");
+        driver.findElement(By.id("t-amount")).sendKeys("1500");
 
         //5. access child iFrame - wiki
         // print listed languages
+        driver.switchTo().parentFrame();
+        driver.switchTo().frame("wikipedia");
+
+        List<WebElement> languages = driver.findElements(By.xpath("//nav/div/a[@class='link-box']"));
+        //languages.forEach(each -> System.out.println(each.getText()));
 
         //6. navigation from one iframe to another, child to parent, etc.
+        driver.switchTo().defaultContent();
 
         //Class Task 1
 
