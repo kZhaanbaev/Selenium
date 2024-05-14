5f81ffa639690aaa4b0827e37e9b339fec0eea1d
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-14 19:17:34 -0400

    day 18

--- src/day18/instructor/_00_Coding_Challenge_Completed.java
+++ src/day18/instructor/_00_Coding_Challenge_Completed.java
@@ -6,4 +6,18 @@ public class _00_Coding_Challenge_Completed {
      * Write a program that takes a number and prints out each digit separately.
      * Example: 4501 -> prints out 4, 5, 0, 1
      */
+    public static void main(String[] args) {
+        printDigits(35482);
+    }
+
+    public static void printDigits(int number){
+        int length = String.valueOf(number).length();
+        int totalZeros = (int)Math.pow(10, length) / 10;
+
+        for (int i = 0; i < length; i++){
+            System.out.println(number / totalZeros);
+            number = number - number /totalZeros * totalZeros;
+            totalZeros /= 10;
+        }
+    }
 }
--- src/day18/instructor/_01_JsExecutor.java
+++ src/day18/instructor/_01_JsExecutor.java
@@ -1,18 +1,42 @@
 package day18.instructor;
 
 import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
 
 public class _01_JsExecutor {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = Driver.getDriver();
         driver.get("https://selenium-practice-app.herokuapp.com/?#/home");
+        Thread.sleep(3000);
 
         //Scroll into view
+        WebElement linksHeaderElement = driver.findElement(By.id("links-header"));
+
+//        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//        jsExecutor.executeScript("arguments[0].scrollIntoView(true)", linksHeaderElement);
+        Driver.moveIntoViewWithJsExecutor(linksHeaderElement);
 
 
-        //highlight element
+        //highlight linksHeaderElement
+//        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//        Thread.sleep(2000);
+//
+//        jsExecutor.executeScript("arguments[0].setAttribute('style','border: solid 2px red');", linksHeaderElement);
+//        jsExecutor.executeScript("arguments[0].style.backgroundColor='yellow'", linksHeaderElement);
+//
+//        Thread.sleep(2000);
+//        jsExecutor.executeScript("arguments[0].setAttribute('style','border: none');", linksHeaderElement);
+//        jsExecutor.executeScript("arguments[0].style.backgroundColor=null", linksHeaderElement);
+        Driver.highlightElement(linksHeaderElement);
 
+        Thread.sleep(3000);
+        driver.close();
     }
 
     //Class Task 1
