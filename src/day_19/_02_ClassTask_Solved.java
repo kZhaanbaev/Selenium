64d3986ec9562faf4e763f55f85ee3fca6c40d82
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-16 17:18:31 -0400

    homework solution

---
+++ src/day_19/_02_ClassTask_Solved.java
@@ -0,0 +1,11 @@
+package day_19;
+
+public class _02_ClassTask_Solved {
+    /**
+     * Run the code Headless
+     *  1. Navigate to https://selenium-practice-app.herokuapp.com/?#/others
+     *  2. Click Toggle button
+     *  3. Verify text of the button on the right changed
+     *  from "Disabled button" to "Enabled button"
+     */
+}
---
+++ src/day_19/_03_ClassTask_Solved.java
@@ -0,0 +1,10 @@
+package day_19;
+
+public class _03_ClassTask_Solved {
+    /**
+     *  1. Open http://automation.techleadacademy.io/#/synchronization
+     *  2. Input City name in Weather Dashboard section
+     *  3. Click Red button
+     *  4. Print out weather details for the city
+     */
+}
