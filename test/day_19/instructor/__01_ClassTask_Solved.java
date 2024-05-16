078327ac6c43819a2aa7d277c0b7ef58fa503f30
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-16 19:08:06 -0400

    day 19

---
+++ test/day_19/instructor/__01_ClassTask_Solved.java
@@ -0,0 +1,10 @@
+package day_18.instructor;
+
+public class __01_ClassTask_Solved {
+    /**
+     * 1. Create 2 methods in this class to handle basic math calculations
+     *    divide(), multiply()
+     *  Create tests for methods divide() and multiply() in _01_ClassTaskTest
+     *  You will need to create the Test class to store test methods.
+     */
+}
---
+++ test/day_19/student/_01_ClassTask.java
@@ -0,0 +1,10 @@
+package day_18.student;
+
+public class _01_ClassTask {
+    /**
+     * 1. Create 2 methods in this class to handle basic math calculations
+     *    divide(), multiply()
+     *  Create tests for methods divide() and multiply() in _01_ClassTaskTest
+     *  You will need to create the Test class to store test methods.
+     */
+}
