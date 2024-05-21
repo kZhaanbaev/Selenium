d3f5227f1cc6e3becec2453f08d31c42126a41d1
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-21 19:26:14 -0400

    day 22

--- test/day_22/instructor/_01_TestNGAnnotations.java
+++ test/day_22/instructor/_01_TestNGAnnotations.java
@@ -1,6 +1,16 @@
 package day_22.instructor;
 
 
+import _utils.Driver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class _01_TestNGAnnotations {
     /**
      6. "invocationCount" - Verify alert is getting displayed within 10 seconds on Synchronization page
@@ -11,4 +21,45 @@ public class _01_TestNGAnnotations {
      substitute WebDriverWait (applies to specific test case, no count needed)
      9. "dataProvider"
      */
+
+    @Test(testName = "Alert display test", invocationCount = 5, invocationTimeOut = 25000L)
+    public void test01(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/synchronization");
+        driver.findElement(By.xpath("//button[@class='btn btn-warning']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+        Driver.closeDriver();
+    }
+
+    @Test(testName = "Timeout Test", timeOut = 5000L)
+    public void test02(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/synchronization");
+        driver.findElement(By.xpath("//button[@class='btn btn-warning']")).click();
+
+        driver.findElement(By.id("input-text")).sendKeys("TEST");
+        driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.textToBe(By.id("text-display"), "TEST"));
+
+        Assert.assertEquals(driver.findElement(By.id("text-display")).getText(),"TEST");
+
+        Driver.closeDriver();
+    }
+
+    @Test(testName = "Expected Exception Test", expectedExceptions = {NoSuchElementException.class}, dependsOnMethods = "Timeout Test")
+    public void test03(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/synchronization");
+
+        driver.findElement(By.linkText("Admin")).click();
+
+        Driver.closeDriver();
+    }
 }
--- test/day_22/instructor/_02_DataProviders.java
+++ test/day_22/instructor/_02_DataProviders.java
@@ -1,6 +1,13 @@
 package day_22.instructor;
 
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class _02_DataProviders {
 
     /*
@@ -10,9 +17,82 @@ public class _02_DataProviders {
      */
 
     //Step 1: without data Providers
+//    @Test(testName = "DataProvider Test on Google page")
+//    public void test01(){
+//        WebDriver driver = Driver.getDriver();
+//        driver.get("https://google.com");
+//        Assert.assertTrue(driver.findElement(By.linkText("Advertising")).isEnabled());
+//        Assert.assertTrue(driver.findElement(By.linkText("Business")).isEnabled());
+//        Assert.assertTrue(driver.findElement(By.linkText("How Search works")).isEnabled());
+//        Assert.assertTrue(driver.findElement(By.linkText("Privacy")).isEnabled());
+//        Assert.assertTrue(driver.findElement(By.linkText("Terms")).isEnabled());
+//        Assert.assertTrue(driver.findElement(By.linkText("Settings")).isEnabled());
+//
+//        Driver.closeDriver();
+//    }
+
+    @Test(testName = "DataProvider Test on Google page")
+    public void test02(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://google.com");
+        Assert.assertTrue(driver.findElement(By.linkText("Advertising")).isEnabled());
+
+        Driver.closeDriver();
+    }
+
+    @Test(testName = "DataProvider Test on Google page")
+    public void test03(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://google.com");
+        Assert.assertTrue(driver.findElement(By.linkText("Business")).isEnabled());
+
+        Driver.closeDriver();
+    }
+
+    @Test(testName = "DataProvider Test on Google page")
+    public void test04(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://google.com");
+        Assert.assertTrue(driver.findElement(By.linkText("Settings")).isEnabled());
+
+        Driver.closeDriver();
+    }
+
+
+    @Test(testName = "DataProvider Test on Google page")
+    public void test05(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://google.com");
+        Assert.assertTrue(driver.findElement(By.linkText("Privacy")).isEnabled());
+
+        Driver.closeDriver();
+    }
+
 
 
     //Step 2: with data Providers
+
+    @DataProvider(name = "data")
+    public Object[] dataLinkTexts(){
+        Object[] data = {
+                "Advertising",
+                "Business",
+                "How Search works",
+                "Privacy",
+                "Terms",
+                "Settings"};
+        return data;
+    }
+
+    @Test(testName = "DataProvider Test on Google page", dataProvider = "data")
+    public void test06(String linkName){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://google.com");
+        Assert.assertTrue(driver.findElement(By.linkText(linkName)).isEnabled());
+
+        Driver.closeDriver();
+    }
+
 
     //ClassTask 1
 
