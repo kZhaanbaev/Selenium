300efcd32f70c636e8e0c5929f4984b656016f57
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-23 20:23:58 -0400

    day23

--- test/day_23/instructor/_01_ClassTask_Solved.java
+++ test/day_23/instructor/_01_ClassTask_Solved.java
@@ -1,5 +1,13 @@
 package day_23.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class _01_ClassTask_Solved {
     //NOTE: Write a Test for each of the scenarios below
 
@@ -11,6 +19,15 @@ public class _01_ClassTask_Solved {
      * 3. Click 'Show message'
      * 4. Verify message is displayed
      */
+    @Test(testName = "US101: Verify Input message")
+    public void test01(){
+        WebDriver driver = Driver.getDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/inputs");
+        driver.findElement(By.id("message")).sendKeys("Java is hard");
+        driver.findElement(By.name("button1")).click();
+        Assert.assertEquals(driver.findElement(By.name("message1")).getText(), "Java is hard");
+    }
 
     /**
      * US102: Verify Input message
@@ -19,6 +36,15 @@ public class _01_ClassTask_Solved {
      * 3. Click 'Show message'
      * 4. Verify message is displayed
      */
+    @Test(testName = "US101: Verify Input message")
+    public void test02(){
+        WebDriver driver = Driver.getDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/inputs");
+        driver.findElement(By.id("message")).sendKeys("It's possible to get better in Java");
+        driver.findElement(By.name("button1")).click();
+        Assert.assertEquals(driver.findElement(By.name("message1")).getText(), "Java is hard");
+    }
 
     /**
      * US103: Verify Input message
@@ -27,6 +53,15 @@ public class _01_ClassTask_Solved {
      * 3. Click 'Show message'
      * 4. Verify message is displayed
      */
+    @Test(testName = "US101: Verify Input message")
+    public void test03(){
+        WebDriver driver = Driver.getDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/inputs");
+        driver.findElement(By.id("message")).sendKeys("I have to try harder to get better");
+        driver.findElement(By.name("button1")).click();
+        Assert.assertEquals(driver.findElement(By.name("message1")).getText(), "Java is hard");
+    }
 
 
 }
