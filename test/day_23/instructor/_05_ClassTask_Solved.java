f2839e06dab16f87e251d61e86300a8824893ae9
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-25 10:38:04 -0400

    day23

--- test/day_23/instructor/_05_ClassTask_Solved.java
+++ test/day_23/instructor/_05_ClassTask_Solved.java
@@ -1,5 +1,17 @@
 package day_23.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class _05_ClassTask_Solved {
 
     /**
@@ -10,6 +22,27 @@ public class _05_ClassTask_Solved {
      * 3. Verify no user is displayed on the table
      * NOTE: test all 3 roles (should have 3 tests in total for US401)
      */
+    @Test(testName = "US401: Verify new user added to the staging table can be erased")
+    public void test01(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/usermgt");
+
+        _04_ClassTask_Solved task4 = new _04_ClassTask_Solved();
+        task4.fillOutRegistrationForm(
+                "John",
+                "Smith",
+                "123-456-7890",
+                "johnsmith@test.com",
+                "Instructor"
+        );
+
+        driver.findElement(By.id("clear-btn")).click();
+        //driver.findElement(By.xpath("//tbody/tr")) vs driver.findElementd(By.xpath("//tbody/tr"))
+        List<WebElement> list = driver.findElements(By.xpath("//tbody/tr"));
+
+        Assert.assertEquals(list.size(), 0);
+        Driver.closeDriver();
+    }
 
 
 
@@ -22,5 +55,33 @@ public class _05_ClassTask_Solved {
      * 4. Verify newly added user in staging table is now added to the database
      * NOTE: test all 3 roles (should have 3 tests in total for US402
      */
+    @Test(testName = "US402: Verify new user is added to the database")
+    public void test02(){
+        WebDriver driver = Driver.getDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/usermgt");
+
+        _04_ClassTask_Solved task4 = new _04_ClassTask_Solved();
+        task4.fillOutRegistrationForm(
+                "John",
+                "Smith",
+                "123-456-7890",
+                "johnsmith@test.com",
+                "Instructor"
+        );
+
+        driver.findElement(By.id("submit-table-btn")).click();
+        driver.findElement(By.linkText("Access DB")).click();
+
+        Set<String> windows = driver.getWindowHandles();
+        String currentWindow = driver.getWindowHandle();
+        for(String each: windows){
+            if (!each.equalsIgnoreCase(currentWindow))
+                driver.switchTo().window(each);
+        }
+
+        Assert.assertTrue(driver.findElement(By.xpath("//td[text()='johnsmith@test.com']")).isDisplayed());
+        Driver.quitDriver();
+    }
 
 }
