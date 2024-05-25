abfb64a74b34a33d2c2176e7c5e1f438ee7c01c5
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-25 09:58:50 -0400

    day23

--- test/day_23/instructor/_04_ClassTask_Solved.java
+++ test/day_23/instructor/_04_ClassTask_Solved.java
@@ -1,5 +1,10 @@
 package day_23.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class _04_ClassTask_Solved {
 
     /**
@@ -7,5 +12,16 @@ public class _04_ClassTask_Solved {
      * First Name, Last Name, Phone Number, Email and Role
      * Using these parameters method should fill out the form and submit
      */
+
+    public void fillOutRegistrationForm(String first, String last, String phone, String email, String role){
+        WebDriver driver = Driver.getDriver();
+        driver.findElement(By.id("Firstname")).sendKeys(first);
+        driver.findElement(By.id("Lastname")).sendKeys(last);
+        driver.findElement(By.id("Phonenumber")).sendKeys(phone);
+        driver.findElement(By.id("Email")).sendKeys(email);
+
+        new Select(driver.findElement(By.id("Select-role"))).selectByVisibleText(role);
+        driver.findElement(By.id("submit-btn")).click();
+    }
 
 }
