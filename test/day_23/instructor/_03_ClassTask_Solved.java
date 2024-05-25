3cc918440ead035a0f8a19ed6f4ec4a6046c4fd9
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-25 09:21:58 -0400

    day23

--- test/day_23/instructor/_03_ClassTask_Solved.java
+++ test/day_23/instructor/_03_ClassTask_Solved.java
@@ -1,5 +1,13 @@
 package day_23.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class _03_ClassTask_Solved {
     //NOTE: Write a Test for each of the scenarios below
 
@@ -11,6 +19,24 @@ public class _03_ClassTask_Solved {
      * 3. Click Submit
      * 4. Verify information is populated on the table below
      */
+    @Test(testName = "US301: Verify Instructor registration")
+    public void test01(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/usermgt");
+        driver.findElement(By.id("Firstname")).sendKeys("John");
+        driver.findElement(By.id("Lastname")).sendKeys("Smith");
+        driver.findElement(By.id("Phonenumber")).sendKeys("123-456-7890");
+        driver.findElement(By.id("Email")).sendKeys("john-smith@test.com");
+
+//        Select select = new Select(driver.findElement(By.id("Select-role")));
+//        select.selectByVisibleText("Instructor");
+
+        new Select(driver.findElement(By.id("Select-role"))).selectByVisibleText("Instructor");
+        driver.findElement(By.id("submit-btn")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//tbody/tr")).isDisplayed());
+        Driver.closeDriver();
+    }
 
 
     /**
@@ -20,6 +46,21 @@ public class _03_ClassTask_Solved {
      * 3. Click Submit
      * 4. Verify information is populated on the table below
      */
+    @Test(testName = "US301: Verify Instructor registration")
+    public void test02(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/usermgt");
+        driver.findElement(By.id("Firstname")).sendKeys("John");
+        driver.findElement(By.id("Lastname")).sendKeys("Smith");
+        driver.findElement(By.id("Phonenumber")).sendKeys("123-456-7890");
+        driver.findElement(By.id("Email")).sendKeys("john-smith@test.com");
+
+        new Select(driver.findElement(By.id("Select-role"))).selectByVisibleText("Mentor");
+        driver.findElement(By.id("submit-btn")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//tbody/tr")).isDisplayed());
+        Driver.closeDriver();
+    }
 
 
     /**
@@ -29,5 +70,20 @@ public class _03_ClassTask_Solved {
      * 3. Click Submit
      * 4. Verify information is populated on the table below
      */
+    @Test(testName = "US301: Verify Instructor registration")
+    public void test03(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://selenium-practice-app.herokuapp.com/?#/usermgt");
+        driver.findElement(By.id("Firstname")).sendKeys("John");
+        driver.findElement(By.id("Lastname")).sendKeys("Smith");
+        driver.findElement(By.id("Phonenumber")).sendKeys("123-456-7890");
+        driver.findElement(By.id("Email")).sendKeys("john-smith@test.com");
+
+        new Select(driver.findElement(By.id("Select-role"))).selectByVisibleText("Student");
+        driver.findElement(By.id("submit-btn")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//tbody/tr")).isDisplayed());
+        Driver.closeDriver();
+    }
 
 }
