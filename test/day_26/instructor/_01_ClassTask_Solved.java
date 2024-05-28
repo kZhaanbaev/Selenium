d1fe68ab0a687b9232f61ef75b5857fc5884226c
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-05-28 19:30:11 -0400

    day26

--- test/day_26/instructor/_01_ClassTask_Solved.java
+++ test/day_26/instructor/_01_ClassTask_Solved.java
@@ -1,5 +1,15 @@
 package day_26.instructor;
 
+import _utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class _01_ClassTask_Solved {
     //NOTE: Write a Test for each of the scenarios below
 
@@ -12,6 +22,22 @@ public class _01_ClassTask_Solved {
      * 4. Click Accounts page button
      * 5. Verify you can see at least 1 record
      */
+    @Test(testName = "US2601: Verify Accounts - Recent items")
+    public void test01(){
+        WebDriver driver = Driver.getDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://techleadacademy-dev-ed.develop.lightning.force.com/lightning/page/home");
+        driver.findElement(By.id("username")).sendKeys("test@techleadacademy.sandbox");
+        driver.findElement(By.id("password")).sendKeys("tech-lead1!");
+        driver.findElement(By.id("Login")).click();
+
+        driver.findElement(By.xpath("//a[@title='Accounts']")).click();
+
+        List<WebElement> list = driver.findElements(By.xpath("//tbody/tr"));
+        Assert.assertTrue(list.size() > 1);
+
+        Driver.closeDriver();
+    }
 
     /**
      * US2602: Verify Accounts - All Accounts
